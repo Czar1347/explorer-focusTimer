8eaf907eb40e18c15f7f0d22b967da25358b4992
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-05-07 15:41:49 -0300

    Adicionando a lógica do ligh mode

---
+++ src/toogle-mode.js
@@ -0,0 +1,15 @@
+let darkmode = true;
+
+const buttonToggle = document.getElementById("toggle-mode");
+
+buttonToggle.addEventListener("click", (event) => {
+  document.documentElement.classList.toggle('light')
+
+  const mode = darkmode ? "light" : "dark";
+  event.currentTarget.querySelector(
+    "span"
+  ).textContent = `${mode} mode ATIVADO`;
+
+  darkmode = !darkmode;
+
+});
