a37728458af6fe5cba8df9ca79f45cb7e20b192a
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-05-12 00:30:25 -0300

    Atualizando o displau do contador

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -9,8 +9,6 @@ export function set(){
 export function reset(){
     state.isRunning = false
     document.documentElement.classList.remove('running')
-
-
 }
 export function toggleMusic(){
     state.isMute = document.documentElement.classList.toggle('music-on')
