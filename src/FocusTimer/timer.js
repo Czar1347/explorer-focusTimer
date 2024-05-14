293c3bd32e2f0d121946180c14b9ca0fe6d5df6c
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-05-14 11:44:12 -0300

    Estruturando o countdown

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,6 +1,24 @@
 import state from './state.js'
 import * as el from './elements.js'
 
+export function countdown(){
+    if(!state.isRunning){
+        return
+    }
+    
+    let minutes = Number(el.minutes.textContent)
+    let seconds = Number(el.seconds.textContent)
+
+    if(seconds == 0){
+
+    }
+    
+    --seconds
+    updateDisplay(minutes, seconds)
+
+    setTimeout(()=>countdown(),1000)
+}
+
 export function updateDisplay(minutes, seconds){
     minutes = minutes ?? state.minutes
     seconds = seconds ?? state.seconds
