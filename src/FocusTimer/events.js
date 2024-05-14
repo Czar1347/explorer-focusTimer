c9b571aee3355924146e0c0a2bc5f413dd6c8ffe
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-05-14 17:35:17 -0300

    Aceitando apenas números no contador

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,5 +1,7 @@
 import { controls } from "./elements.js";
 import * as actions from "./actions.js"
+import * as el from "./elements.js"
+
 
 
 export function registerControls(){
@@ -9,5 +11,11 @@ export function registerControls(){
         return
        }
        actions[action]()
+    })
+}
+
+export function setMinutes(){
+    el.minutes.addEventListener('focus',()=>{
+        el.minutes.textContent = ""
     })
 }
